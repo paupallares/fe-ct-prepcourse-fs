930b5fe8f50bd133e961031a3b4b38f4efb6081c
paupallares <ppaupallaresgmail.com>
2023-01-13 13:17:09 +1000

    homework 04

--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -19,7 +19,9 @@ function tienenMismaLongitud(str1, str2) {
    if (str1.lenght === str2.lenght) {
       return true;
    }
+   if (str1.lenght <= str2.lenght) {
    return false;
+   }
 }
 
 function menosQueNoventa(num) {
